4802e01379cd773a0af2bde467957c44125b273d
Quinten Kock <quinten@quinten.space>
2017-06-02 17:31:09 +0200

    Begin work on the database connector

---
+++ backend/src/dbconnector.py
@@ -0,0 +1,12 @@
+import mysql.connector as mariadb
+
+class dbconnector():
+    def __init__(self, name, passw, database)
+        self.db = mariadb.connect(user,passw,database)
+        self.cursor = self.db.cursor()
+    def getArticle(self, aid)
+        self.cursor.execute("SELECT WriterUID, title, subtitle, submitdate, summary, body FROM Articles WHERE AID=%s", (aid,))
+        article = self.cursor.fetchone()
+	WriterUID, title, subtitle, submitdate, summary, body = article
+	       
+        
