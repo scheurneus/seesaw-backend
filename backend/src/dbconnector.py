7b8c7884099abf23facc73f8d5eb5cb4169bbc9f
Quinten Kock <quinten@quinten.space>
2017-06-02 17:46:31 +0200

    Add smart displayname retrieval

--- backend/src/dbconnector.py
+++ backend/src/dbconnector.py
@@ -1,12 +1,23 @@
 import mysql.connector as mariadb
 
 class dbconnector():
-    def __init__(self, name, passw, database)
-        self.db = mariadb.connect(user,passw,database)
+    def __init__(self, name, passw, dbase)
+        self.db = mariadb.connect(username=user,password=passw,database=dbase)
         self.cursor = self.db.cursor()
-    def getArticle(self, aid)
+    
+    def getDisplayName(self,uid):
+        self.cursor.execute("SELECT displayname, username FROM Users WHERE UID=%s",(uid,))
+        displayname, username = self.cursor.fetchone()
+        self.cursor.reset()
+        if displayname:
+            return displayname
+        else:
+            return username
+    
+    def getArticle(self, aid):
         self.cursor.execute("SELECT WriterUID, title, subtitle, submitdate, summary, body FROM Articles WHERE AID=%s", (aid,))
         article = self.cursor.fetchone()
+        self.cursor.reset()
         WriterUID, title, subtitle, submitdate, summary, body = article
-           
-        
+        author = getDisplayName(WriterUID)
+    
