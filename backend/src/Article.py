08980a7667a97eea427917fbe7ba058e24a47275
Nicolas Manichon <nicolas.manichon@epita.fr>
2017-06-02 15:28:24 +0200

    Added the article class

---
+++ backend/src/Article.py
@@ -0,0 +1,10 @@
+class Article:
+    '''
+    Represents an article and its properties
+    '''
+
+    def __init__(self, title, author, date, formatting):
+        self.Title = title
+        self.Author = author
+        self.Date = date
+        self.Formatting = formatting
