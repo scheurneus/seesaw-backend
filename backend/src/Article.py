f5b97a2b4b45fcc751c4464f2cd83a0d44404e73
pinoaffe <pinoaffe@airmail.cc>
2017-06-06 16:47:12 +0200

    made db_connector.py pep8 compliant

--- backend/src/Article.py
+++ backend/src/Article.py
@@ -3,10 +3,11 @@ class Article:
     Represents an article and its properties
     '''
 
-    def __init__(self, article_id, title, subtitle, author, date, content):
+    def __init__(self, article_id, title, subtitle, author_id, author, date, content):
         self.article_id = article_id
         self.title = title
         self.subtitle = subtitle
+        self.author_id = author_id
         self.author = author
         self.date = date
         self.content = content
--- backend/src/db_connector.py
+++ backend/src/db_connector.py
@@ -3,21 +3,22 @@ import mysql.connector as mariadb
 from passlib.apps import custom_app_context as pwd_context
 from Config import config
 from Article import Article
-from json import dumps
+
 
 class db_connector():
     def __init__(self):
         self.db = mariadb.connect(user=config['DB_USER'],
                                   password=config['DB_PASS'],
                                   database=config['DB_NAME'])
-                                  
+
         self.cursor = self.db.cursor()
-        self.db.paramstyle='format'
+        self.db.paramstyle = 'format'
+
     # ARTICLE MANAGEMENT
     def get_article(self, article_id):
-        # gets an article from the database, 
+        # gets an article from the database,
         # returns a dictionary with article content and everything
-        #try:
+        # try:
         self.cursor.execute(
             "SELECT WriterUID, title, subtitle, submitdate, summary, body FROM Articles WHERE AID = %s",
             (article_id,))
@@ -29,15 +30,16 @@ class db_connector():
             title,
             subtitle,
             writer_uid,
+            writer,
             submitdate,
             body
         )
-        #except Exception as e: 
-            #return False
+        # except Exception as e:
+        #   return False
 
     def push_article(self, writer_uid, title, subtitle, summary, body, link_ids=[], tags=[]):
-        # adds a new article to the database, 
-        # returns a bool describing whether the action was successfull and a second field containing either the reason for failure or the article's newly generated article id upon success 
+        # adds a new article to the database,
+        # returns a bool describing whether the action was successfull and a second field containing either the reason for failure or the article's newly generated article id upon success
         submitdate = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())
         try:
             self.cursor.execute(
@@ -52,45 +54,46 @@ class db_connector():
             return True, article_id
         except:
             return False, "query execution failed"
+
     def delete_article(self, article_id):
         pass
 
     # ARTICLE LISTS
     def get_article_list(self, method, amount, start, origin=False):
-        if method=="parents":
+        if method == "parents":
             try:
-                self.cursor.execute("SELECT ChildAID FROM Links WHERE ParentAID=%s",(aid,))
+                self.cursor.execute("SELECT ChildAID FROM Links WHERE ParentAID=%s", (origin,))
                 return [i[0] for i in self.cursor.fetchall()]
             except:
                 return False
-        elif method=="children":
+        elif method == "children":
             try:
-                self.cursor.execute("SELECT ParentAID FROM Links WHERE ChildAID=%s",(aid,))
+                self.cursor.execute("SELECT ParentAID FROM Links WHERE ChildAID=%s", (origin,))
                 return [i[0] for i in self.cursor.fetchall()]
             except:
                 return False
-        elif method=="tagged":
+        elif method == "tagged":
             try:
-                self.cursor.execute("SELECT AID FROM Tags WHERE tag=%s",(origin,))
+                self.cursor.execute("SELECT AID FROM Tags WHERE tag=%s", (origin,))
                 return [self.get_article(i[0]) for i in self.cursor.fetchall()]
             except:
                 return False
-        elif method=="newest":
+        elif method == "newest":
             pass
-        elif method=="oldest":
+        elif method == "oldest":
             pass
-        elif method=="controversial":
+        elif method == "controversial":
             pass
         else:
             raise ValueError("This method doesn't exist")
 
     # USER MANAGEMENT
-    def get_displayname(self,uid):
+    def get_displayname(self, uid):
         # gets the name to be displayed for a given user id
-        # returns a string with displayname or username if the user has no displayname. If the user doesn't exist, it returns false. 
-        if not self.check_user_exists(user_id = uid):
+        # returns a string with displayname or username if the user has no displayname. If the user doesn't exist, it returns false.
+        if not self.check_user_exists(user_id=uid):
             return False
-        self.cursor.execute("SELECT displayname, username FROM Users WHERE UID = %s",(uid,))
+        self.cursor.execute("SELECT displayname, username FROM Users WHERE UID = %s", (uid,))
         displayname, username = self.cursor.fetchone()
         self.cursor.reset()
         if displayname:
@@ -98,11 +101,11 @@ class db_connector():
         return username
 
     def push_user(self, username, password, email, displayname=""):
-    # adds a new user account to the database, 
-    # returns a bool describing whether the action was successfull and in the second field the newly generated user id upon success or the reason for failure upon failure
+        # adds a new user account to the database,
+        # returns a bool describing whether the action was successfull and in the second field the newly generated user id upon success or the reason for failure upon failure
         password = pwd_context.hash(password)
         regdate = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())
-        if self.check_user_exists(username = username):
+        if self.check_user_exists(username=username):
             raise ValueError("a user with the given username already exists")
         try:
             self.cursor.execute(
@@ -114,12 +117,12 @@ class db_connector():
             return False, "query execution failed"
 
     def modify_user(self, user_id, username=False, password=False, email=False, displayname=False):
-    # modifies the properties of a preexisting user acocunt, 
-    # returns True if all goes well
-    # server.py needs to check whether the user already has a session before executing this function
-        if self.check_user_exists(username = username):
+        # modifies the properties of a preexisting user acocunt,
+        # returns True if all goes well
+        # server.py needs to check whether the user already has a session before executing this function
+        if self.check_user_exists(username=username):
             raise ValueError("a user with the given username already exists")
-        #still need to update this so it doesn't fail when people try to input their own username
+        # still need to update this so it doesn't fail when people try to input their own username
         if username:
             try:
                 self.cursor.execute("UPDATE Users SET username=%s WHERE UID=%s", (username, user_id))
@@ -142,10 +145,9 @@ class db_connector():
                 raise IOError("displayname query failed")
 
     def check_user_exists(self, username=False, user_id=False):
-    # checks whether a user exists with the properties in the variables, 
-    # returns bool
-
-    # todo: maybe add email too?
+        # checks whether a user exists with the properties in the variables
+        # returns bool
+        # todo: maybe add email too?
         if username and user_id:
             self.cursor.execute(
                 "SELECT UID from Users where username=%s and UID=%s",
@@ -164,9 +166,9 @@ class db_connector():
         return False
 
     def check_password(self, username, password):
-    # checks whether the supplied password is correct, 
-    # returns bool 
-        if not self.check_user_exists(username = username):
+        # checks whether the supplied password is correct,
+        # returns bool
+        if not self.check_user_exists(username=username):
             raise ValueError("no user with the given username exists")
         self.cursor.execute("SELECT password from Users where username=%s", (username,))
         fetched_hash = self.cursor.fetchone()[0]
